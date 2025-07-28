96072b4b995b20d1c9e324be5a009e982cceae35
MajesticPie <163900819+MajesticPie@users.noreply.github.com>
2025-07-28 03:25:25 +0100

    update api

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
+    apiKey: "AIzaSyAHlhlkT3qr-BnAUmUkmL7q8O-sS8IGbfM",
+    authDomain: "skilltree-7d36e.firebaseapp.com",
+    projectId: "skilltree-7d36e",
+    storageBucket: "skilltree-7d36e.firebasestorage.app",
+    messagingSenderId: "933469258781",
+    appId: "1:933469258781:web:920cc7d096ac96890a32f4",
   };
 
 const app = initializeApp(firebaseConfig);
